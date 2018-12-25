dd0e822c8af240b45c13b4e3e63ffeced5ebd1d1
Ryotaman213 <ryotafreeman@gmail.com>
2018-12-25 20:15:48 +0900

    initialcommit

---
+++ TrickGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrickGames
+//
+//  Created by 小川 僚太 on 2018/12/25.
+//  Copyright © 2018 Ryota Ogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
